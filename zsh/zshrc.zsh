4b9035ffee1b950c1963a37408cba6910daf3cf5
Aung Myo Kyaw <aungmyokyaw.git@icloud.com>
2022-02-15 21:22:03 +0630

    arrgghh... damn this thing for not working.

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -13,13 +13,9 @@ source ~/.options.zsh
 source ~/.theme.zsh
 source ~/.zsh_plugins.sh
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
 eval $(thefuck --alias)
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
